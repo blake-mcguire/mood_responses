46e5118f50dd6390884cdd139b7ba979b259aad2
blake-mcguire <blake.lm@outlook.com>
2024-06-27 16:47:46 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import mood
+
+mood_today = input('How are you feeling today?: ').lower()
+
+mood.mood_responses(mood_today)
---
+++ mood.py
@@ -0,0 +1,8 @@
+def mood_responses(mood):
+    if mood == 'sad':
+        print('sorry you feel that way')
+    elif mood == 'happy':
+        print('Glad you are feeling good!')
+    else:
+        print('Wow so emotionally deep and mysterious...')
+        
